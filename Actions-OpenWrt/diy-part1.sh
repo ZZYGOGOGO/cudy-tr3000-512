43342d33822928a2aa1071587039f91b98e839e4
GitHub Action <action@github.com>
2026-09-13 16:58:29 +0800

    fix: remove ;main suffix from feed URLs and add feeds update retry logic
    - diy-part1.sh: use bare repo URLs without branch suffix to avoid feeds script parse issue
    - workflows: add 3-attempt retry loop for feeds update step to handle transient network failures

--- Actions-OpenWrt/diy-part1.sh
+++ Actions-OpenWrt/diy-part1.sh
@@ -14,5 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add PassWall2 feed sources
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >> feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >> feeds.conf.default
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >> feeds.conf.default
